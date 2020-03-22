529937e0849e99534d4cce3085d99b5bf5b89745
Juha Kuitunen <kuitsi@iki.fi>
2020-03-22 20:23:08 +0200

    3.6 require only characters a-z in username

--- viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -40,7 +40,7 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
-        if (username.length() < 3) return true;
+        if (!username.matches("^[a-z]{3,}$")) return true;
         if (password.length() < 8) return true;
 
         return false;
