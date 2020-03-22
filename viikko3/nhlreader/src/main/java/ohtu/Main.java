f2ebfcccf69b9d63e46f549c2daab8896e955650
Juha Kuitunen <kuitsi@iki.fi>
2020-03-22 17:46:35 +0200

    3.2 sort by decreasing total points

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,7 +1,11 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
@@ -14,7 +18,19 @@ public class Main {
         System.out.println( bodyText );
 
         Gson mapper = new Gson();
-        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        List<Player> players = mapper.fromJson(bodyText, new TypeToken<ArrayList<Player>>(){}.getType());
+        players.sort(new Comparator<Player>() {
+            // decreasing total points
+            @Override
+            public int compare(Player p1, Player p2) {
+                int p1TotalPoints = p1.getGoals() + p1.getAssists();
+                int p2TotalPoints = p2.getGoals() + p2.getAssists();
+                if (p1TotalPoints == p2TotalPoints) {
+                    return 0;
+                }
+                return p1TotalPoints > p2TotalPoints ? -1 : 1;
+             }
+        });
 
         System.out.println("Players from FIN:");
         for (Player player : players) {
