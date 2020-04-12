fe9621fd6e5c5b2a36a107aac6dd071797536af9
Juha Kuitunen <kuitsi@iki.fi>
2020-04-12 19:29:00 +0300

    4.2 add new tests

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,18 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void kortilleLadataanJosRahaaOn() {
+        int ladattava = 15;
+        kassa.lataa(kortti, ladattava);
+        verify(kortti, times(1)).lataa(ladattava);
+    }
+
+    @Test
+    public void kortiltaEiVelotetaJosLatausSummaNegatiivinen() {
+        kassa.lataa(kortti, -5);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
+
 }
