6b71063fa9ea0b341bbe5a2dfc449d26f3e104ab
Juha Kuitunen <kuitsi@iki.fi>
2020-04-13 12:28:33 +0300

    4.3 add new test for buying two different items that are in stock

--- viikko2/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko2/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -58,4 +58,24 @@ public class KauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla arvoilla
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
     }
+
+    @Test
+    public void kahdenTuotteenOstossaPankinMetodiaTilisiirtoKutsutaanOikeillaArvoilla() {
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 6));
+
+        // sitten testattava kauppa
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2); // leipä
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla arvoilla
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(11));
+    }
 }
