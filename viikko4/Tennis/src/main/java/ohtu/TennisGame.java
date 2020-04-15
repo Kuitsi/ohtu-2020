d07449ebce0061a9ecc130f1aa9b903a7188f3e8
Juha Kuitunen <kuitsi@iki.fi>
2020-04-15 19:04:00 +0300

    4.6 even scores

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,15 +1,24 @@
 package ohtu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGame {
 
     private int player1score = 0;
     private int player2score = 0;
     private final String player1Name;
     private final String player2Name;
+    private final Map<Integer, String> scoreNames = new HashMap<>();
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+
+        scoreNames.put(0, "Love");
+        scoreNames.put(1, "Fifteen");
+        scoreNames.put(2, "Thirty");
+        scoreNames.put(3, "Forty");
     }
 
     public void wonPoint(String playerName) throws IllegalArgumentException {
@@ -22,29 +31,22 @@ public class TennisGame {
         }
     }
 
+    private String getEvenScore() {
+        if (scoreNames.containsKey(player1score)) {
+            return scoreNames.get(player1score) + "-All";
+        } else {
+            return "Deuce";
+        }
+    }
+
     public String getScore() {
+        if (player1score == player2score) {
+            return getEvenScore();
+        }
+
         String score = "";
         int tempScore = 0;
-        if (player1score == player2score) {
-            switch (player1score) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (player1score >= 4 || player2score >= 4) {
+        if (player1score >= 4 || player2score >= 4) {
             int minusResult = player1score - player2score;
             if (minusResult == 1) {
                 score = "Advantage " + player1Name;
