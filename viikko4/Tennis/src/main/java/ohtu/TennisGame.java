47481be38485ab79f01a87149eb5848109a66aae
Juha Kuitunen <kuitsi@iki.fi>
2020-04-15 18:34:09 +0300

    4.6 use player name variables and also compare them properly

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -4,19 +4,21 @@ public class TennisGame {
 
     private int player1score = 0;
     private int player2score = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1") {
+    public void wonPoint(String playerName) throws IllegalArgumentException {
+        if (playerName.equalsIgnoreCase(player1Name)) {
             player1score += 1;
-        } else {
+        } else if (playerName.equalsIgnoreCase(player2Name)) {
             player2score += 1;
+        } else {
+            throw new IllegalArgumentException("Name must be " + player1Name + " or " + player2Name);
         }
     }
 
@@ -45,13 +47,13 @@ public class TennisGame {
         } else if (player1score >= 4 || player2score >= 4) {
             int minusResult = player1score - player2score;
             if (minusResult == 1) {
-                score = "Advantage player1";
+                score = "Advantage " + player1Name;
             } else if (minusResult == -1) {
-                score = "Advantage player2";
+                score = "Advantage " + player2Name;
             } else if (minusResult >= 2) {
-                score = "Win for player1";
+                score = "Win for " + player1Name;
             } else {
-                score = "Win for player2";
+                score = "Win for " + player2Name;
             }
         } else {
             for (int i = 1; i < 3; i++) {
