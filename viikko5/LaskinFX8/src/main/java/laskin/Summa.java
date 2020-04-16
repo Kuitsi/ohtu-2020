0488be005c81483995dd87edede0ef04b96d01a0
Juha Kuitunen <kuitsi@iki.fi>
2020-04-16 23:02:41 +0300

    5.4 add commands

---
+++ viikko5/LaskinFX8/src/main/java/laskin/Summa.java
@@ -0,0 +1,17 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = haeSyote();
+        sovellus.plus(arvo);
+        paivitaKentat();
+    }
+}
