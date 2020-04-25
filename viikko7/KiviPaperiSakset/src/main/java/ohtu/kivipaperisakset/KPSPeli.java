f157e7db5dd601822df41ec341f3beb4117714b9
Juha Kuitunen <kuitsi@iki.fi>
2020-04-25 23:10:11 +0300

    7.3 add factory method for creating new game and remove concrete dependencies from main program

--- viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSPeli.java
+++ viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSPeli.java
@@ -12,7 +12,11 @@ public abstract class KPSPeli {
         this.tuomari = new Tuomari();
     }
 
+    /**
+     * Aloittaa pelin
+     */
     public void pelaa() {
+        io.tulosta("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
         teeSiirrot();
 
         while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
@@ -40,4 +44,28 @@ public abstract class KPSPeli {
     }
 
     protected abstract String pelaajan2siirto();
+
+    /**
+     * Kysyy pelattavan pelin tyypin ja valmistelelee sen aloitettavaksi {@link #pelaa} metodilla
+     * @return uusi KPSPeli tai null mikäli valittu muuta kuin annetut vaihtoehdot
+     */
+    public static KPSPeli luoUusiPeli() {
+        IO io = new KonsoliIO();
+
+        io.tulosta("\nValitse pelataanko"
+                + "\n (a) ihmistä vastaan"
+                + "\n (b) tekoälyä vastaan"
+                + "\n (c) parannettua tekoälyä vastaan"
+                + "\nmuilla valinnoilla lopetetaan");
+
+        String vastaus = io.seuraava();
+        if (vastaus.endsWith("a")) {
+            return new KPSPelaajaVsPelaaja(io);
+        } else if (vastaus.endsWith("b")) {
+            return new KPSTekoaly(io);
+        } else if (vastaus.endsWith("c")) {;
+            return new KPSParempiTekoaly(io);
+        }
+        return null;
+    }
 }
--- viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
+++ viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
@@ -3,32 +3,10 @@ package ohtu.kivipaperisakset;
 public class Paaohjelma {
 
     public static void main(String[] args) {
-        IO io = new KonsoliIO();
-
         while (true) {
-            io.tulosta("\nValitse pelataanko"
-                    + "\n (a) ihmistä vastaan "
-                    + "\n (b) tekoälyä vastaan"
-                    + "\n (c) parannettua tekoälyä vastaan"
-                    + "\nmuilla valinnoilla lopetataan");
-
-            String vastaus = io.seuraava();
-            if (vastaus.endsWith("a")) {
-                io.tulosta("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSPeli kaksinpeli = new KPSPelaajaVsPelaaja(io);
-                kaksinpeli.pelaa();
-            } else if (vastaus.endsWith("b")) {
-                io.tulosta("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSPeli yksinpeli = new KPSTekoaly(io);
-                yksinpeli.pelaa();
-            } else if (vastaus.endsWith("c")) {
-                io.tulosta("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSPeli pahaYksinpeli = new KPSParempiTekoaly(io);
-                pahaYksinpeli.pelaa();
-            } else {
-                break;
-            }
-
+            KPSPeli peli = KPSPeli.luoUusiPeli();
+            if (peli == null) break;
+            peli.pelaa();
         }
 
     }
